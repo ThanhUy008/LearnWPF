9560cfc71f255743d0a50515ce97d5c3e9d08714
thanhuy008 <thanhuy008@gmail.com>
2021-09-28 15:52:12 +0700

    Tuan03

---
+++ Tuan03/Login/Login/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Login
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(String.IsNullOrEmpty(Username.Text))
+            {
+                MessageBox.Show("Không được để trống tài khoản","Thông báo",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+            else if (String.IsNullOrEmpty(Password.Password))
+            {
+                MessageBox.Show("Không được để trống mật khẩu", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+            else if(Username.Text.Equals("admin")&& Password.Password.Equals("qwe3@1")){
+
+                var dashboard = new Dashboard();
+                dashboard.Show();
+            }
+            else
+            {
+                MessageBox.Show("Sai tài khoản", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+        }
+    }
+}
---
+++ Tuan03/StudentBinding/StudentBinding/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var sv = new Student
+            {
+                ID = "18120645",
+                Name = "Bùi Thanh Uy",
+                Address = "Tổ 3, Phường 8, Quận Tân Bình",
+                Email = "thanhuy008@gmail.com",
+                Phone = "0123456778"
+            };
+            this.DataContext = sv;
+        }
+    }
+}
